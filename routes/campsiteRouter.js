2a39d7f77d6cb90184a3d6c9ee77369658b29878
Jame Chaney <macneilslt@gmail.com>
2023-07-11 22:15:20 -0400

    added params to .put on routes

---
+++ routes/campsiteRouter.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const campsiteRouter = express.Router();
+
+campsiteRouter
+	.route('/')
+	.all((req, res, next) => {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/plain');
+		next();
+	})
+	.get((req, res) => {
+		res.end('Will send all the campsites to you');
+	})
+	.post((req, res) => {
+		res.end(
+			`Will add the campsite: ${req.body.name} with description: ${req.body.description}`
+		);
+	})
+	.put((req, res) => {
+		res.statusCode = 403;
+		res.end('PUT operation not su:campsiteIdpported on /campsites');
+	})
+	.delete((req, res) => {
+		res.end('Deleting all campsites');
+	});
+
+campsiteRouter
+	.route('/:campsiteId')
+	.all((req, res, next) => {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/plain');
+		next();
+	})
+	.get((req, res) => {
+		res.end(`Will send you campsite with id of ${req.params.campsiteId}`);
+	})
+	.post((req, res) => {
+		res.statusCode = 403;
+		res.end('Not supported');
+	})
+	.put((req, res) => {
+		res.end(
+			`Will update the campsite:${req.params.campsiteId} ${req.body.name} and description: ${req.body.description}`
+		);
+	})
+	.delete((req, res) => {
+		res.end(`Deleting campsite with id: ${req.params.campsiteId}`);
+	});
+
+module.exports = campsiteRouter;
